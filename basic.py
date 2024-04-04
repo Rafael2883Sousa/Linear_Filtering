2f86929d4c8fdf10a993b79d558dc3375f0282d2
Rafael Souza <rafs2883.1@gmail.com>
2024-04-04 16:04:27 -0100

    Un-comenting

--- basic.py
+++ basic.py
@@ -1,19 +1,19 @@
 import cv2 as cv
 
-img = cv.imread('Images/Cat_3.jpg')
-#cv.imshow('Cat', img)
+img = cv.imread('Images/cat_1.jpg')
+cv.imshow('Cat', img)
 
 # Converting to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-#cv.imshow('Gray', gray)
+cv.imshow('Gray', gray)
 
 # Blur (removes noises that exist in a image)
 blur = cv.GaussianBlur(img, (3, 3), cv.BORDER_DEFAULT)
-#cv.imshow('Blur', blur)
+cv.imshow('Blur', blur)
 
 # Edge Cascade (canny detector)
 canny = cv.Canny(blur, 125, 175)  # edges present in the image
-#cv.imshow('Canny', canny)
+cv.imshow('Canny', canny)
 
 # Dilating the image
 dilated = cv.dilate(canny, (3, 3), iterations=1)
@@ -29,6 +29,6 @@ resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
 
 # Cropping
 cropped = img[50: 200, 200:400]
-cv.imshow('Cropped', cropped)
+#cv.imshow('Cropped', cropped)
 
 cv.waitKey(0)
